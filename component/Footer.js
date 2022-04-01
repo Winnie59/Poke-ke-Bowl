84a1c5375d7e05c60c3152222e63c2dbd60af03d
winnie59 <kwang_kwah_noi@yahoo.com>
2022-04-01 13:13:21 -0400

    clean up

--- component/Footer.js
+++ component/Footer.js
@@ -7,7 +7,6 @@ const Footer = () => {
   return (
     <div className={styles.container} id='contact'>
       <div className={styles.item}>
-        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
       </div>
       <div className={styles.item}>
         <div className={styles.card}>
@@ -33,11 +32,10 @@ const Footer = () => {
     
         </div>
         <div className={styles.card}>
-        < Link href='https://www.google.com/maps/dir//121+Harvard+Ave,+Allston,+MA+02134/@42.3524172,-71.1343685,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89e379c5901d9357:0x8f1154fd5b4e28a8!2m2!1d-71.1321798!2d42.3524172'  passHref >
-          <a target='_blank'>
+          
+          <a href='https://www.google.com/maps/dir//121+Harvard+Ave,+Allston,+MA+02134/@42.3524172,-71.1343685,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89e379c5901d9357:0x8f1154fd5b4e28a8!2m2!1d-71.1321798!2d42.3524172' target='_blank'>
             <Image className={styles.map} src='/img/map.png'objectFit="contain" width={300} height={300} alt="" />
           </a>
-        </Link>
         </div>
       </div>
     </div>
