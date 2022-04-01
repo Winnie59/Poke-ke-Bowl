e9e17aedc7ae4fb06e36eb625c6df126969c72af
winnie59 <kwang_kwah_noi@yahoo.com>
2022-04-01 15:16:56 -0400

    add config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  /* config options here */
+}
+
+module.exports = nextConfig
+
+
 module.exports = {
   images:{
     domains:['res.cloudinary.com']
