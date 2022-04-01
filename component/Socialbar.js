ed220f4e8a9d584a139e8ed656961de29a721e08
winnie59 <kwang_kwah_noi@yahoo.com>
2022-04-01 08:39:24 -0400

    add ityped

--- component/Socialbar.js
+++ component/Socialbar.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from '../styles/Navbar.module.css'
+import { init } from 'ityped'
 
 const Socialbar = () => {
+
+  const textRef = useRef()
+
+  useEffect(()=> {
+    init(textRef.current, { 
+      showCursor: false,
+      backDelay: 1500, 
+      backSpeed: 50,
+      strings: ['FREE DELIVERY AT $35', 'ONLINE $5 OFF', 'ORDER NOW!!' ] 
+    })
+  },[])
+
   return (
     <div className={styles.socialCall}>
         <div className={styles.social}>
         
         </div>
         <div className={styles.phone}>
-            <span>Call (559)-691-9294</span>
+            <span className={styles.call}>Call (559)-691-9294</span>
+            <span ref={textRef}></span>
         </div>
     </div>
   )
