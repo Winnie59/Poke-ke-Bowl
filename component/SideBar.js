042a89e139a1b9a8954b41e933811c921efece81
winnie59 <kwang_kwah_noi@yahoo.com>
2022-04-02 02:15:02 -0400

    updated side bar

--- component/SideBar.js
+++ component/SideBar.js
@@ -68,7 +68,9 @@ const SideBar = ({sideOpen, setSideOpen}) => {
           <Link href='/#menu' passHref>
           <li className={styles.listItem}>Menu</li>
           </Link>
-          <li className={styles.listItem}>Pick Poke</li>
+          <Link href='#contact' passHref>
+          <li className={styles.listItem}>Find Us</li>
+          </Link>
           <Link href='#contact' passHref>
           <li className={styles.listItem}>Contact</li>
           </Link>
